b717dc28027f50584875c99965a2ae090017a090
titch <sujinlee0202@gmail.com>
2023-10-24 16:39:01 +0900

    Add Stack, Queue

---
+++ queue.js
@@ -0,0 +1,35 @@
+class Queue {
+  arr = [];
+
+  enqueue(value) {
+    this.arr.push(value);
+  }
+
+  dequeue() {
+    this.arr.shift();
+  }
+
+  peek() {
+    // first, 맨 앞에 값
+    return this.arr[0];
+  }
+
+  get length() {
+    return this.arr.length;
+  }
+}
+
+const queue = new Queue();
+
+queue.enqueue(1);
+queue.enqueue(3);
+queue.enqueue(5);
+queue.enqueue(2);
+queue.enqueue(4);
+
+console.log(queue.length); // 5 출력
+console.log(queue.peek()); // 1 출력
+
+queue.dequeue();
+
+console.log(queue.peek()); // 3 출력
---
+++ stack.js
@@ -0,0 +1,35 @@
+class Stack {
+  arr = [];
+
+  push(value) {
+    this.arr.push(value);
+  }
+
+  pop() {
+    this.arr.pop();
+  }
+
+  // stack에서 가장 위의 데이터
+  top() {
+    return this.arr.at(-1); // this.arr[this.arr.length-1]
+  }
+
+  get length() {
+    return this.arr.length;
+  }
+}
+
+const stack = new Stack();
+
+stack.push(1);
+stack.push(3);
+stack.push(5);
+stack.push(2);
+stack.push(4);
+
+console.log(stack.length); // 5 출력
+console.log(stack.top()); // 4 출력
+
+stack.pop();
+
+console.log(stack.top()); // 2 출력
